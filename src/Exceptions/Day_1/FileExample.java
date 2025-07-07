36a06e07f8c9bbae9c20d7e2a4ea18f6118c07df
meAltf <alataf.dce.cs1@gmail.com>
2025-07-07 20:56:14 +0530

    Checked Exception | FileFileNotFoundException

---
+++ src/Exceptions/Day_1/FileExample.java
@@ -0,0 +1,15 @@
+package Exceptions.Day_1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileExample {
+
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("Nonexistent.txt");
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("File not found!");
+        }
+    }
+}
