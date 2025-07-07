b5309283a5d090eed63f6579798d5ce2e2fc9d11
meAltf <alataf.dce.cs1@gmail.com>
2025-07-07 20:44:17 +0530

    Unchecked | ArrrayIndexOutOfBoundException

---
+++ src/Exceptions/Day_1/ArrayExample.java
@@ -0,0 +1,8 @@
+package Exceptions.Day_1;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        System.out.println(arr[5]); // ❌ Out of bounds
+    }
+}
