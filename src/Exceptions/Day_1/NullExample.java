0c12d68adbcca69d0ce134f2617e609dd2116afa
meAltf <alataf.dce.cs1@gmail.com>
2025-07-07 20:43:02 +0530

    Checked | Null pointer exception

---
+++ src/Exceptions/Day_1/NullExample.java
@@ -0,0 +1,8 @@
+package Exceptions.Day_1;
+
+public class NullExample {
+    public static void main(String[] args) {
+        String name = null;
+        System.out.println(name.length()); // ❌ NullPointerException
+    }
+}
