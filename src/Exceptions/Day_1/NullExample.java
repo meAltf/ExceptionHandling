5ec2fdb52fdd187b64beeee0f778de181e109fe0
meAltf <alataf.dce.cs1@gmail.com>
2025-07-07 20:45:16 +0530

    Unchecked | NullPointerException

--- src/Exceptions/Day_1/NullExample.java
+++ src/Exceptions/Day_1/NullExample.java
@@ -3,6 +3,6 @@ package Exceptions.Day_1;
 public class NullExample {
     public static void main(String[] args) {
         String name = null;
-        System.out.println(name.length()); // ❌ NullPointerException
+        System.out.println(name.length()); // ❌ NullPointerException | UnCheckedException
     }
 }
